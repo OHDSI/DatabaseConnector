2e161cf1e5058be96c357f4944f40192930d6edc
schuemie <schuemie@ohdsi.org>
2014-04-04 09:59:10 +0200

    Some bugfixes

--- R/DatabaseConnector.R
+++ R/DatabaseConnector.R
@@ -40,21 +40,23 @@
 #' security, the domain can be specified using <mydomain>/<user>.
 #' @param password			The password for that user
 #' @param server			The name or IP-address of the server
+#' @param port				(optional) The port on the server to connect to
 #' @param schema			(optional) The name of the schema to connect to
 #' @return              
 #' An object that extends \code{DBIConnection} in a database-specific manner. This object is used to direct commands to the database engine. 
-#' @examples
-#' conn <- connect(dbms="mysql", server="localhost",user="root",password="xxx",schema="cdm_v4")
-#' dbGetQuery(conn,"SELECT COUNT(*) FROM person")
-#' dbDisconnect(conn)
+#' @examples \dontrun{
+#'   conn <- connect(dbms="mysql", server="localhost",user="root",password="xxx",schema="cdm_v4")
+#'   dbGetQuery(conn,"SELECT COUNT(*) FROM person")
+#'   dbDisconnect(conn)
 #' 
-#' conn <- connect(dbms="sql server", server="RNDUSRDHIT06.jnj.com",schema="Vocabulary")
-#' dbGetQuery(conn,"SELECT COUNT(*) FROM concept")
-#' dbDisconnect(conn)
+#'   conn <- connect(dbms="sql server", server="RNDUSRDHIT06.jnj.com",schema="Vocabulary")
+#'   dbGetQuery(conn,"SELECT COUNT(*) FROM concept")
+#'   dbDisconnect(conn)
 #' 
-#' conn <- connect(dbms="oracle", server="127.0.0.1:1521/xe",user="system",password="xxx",schema="test")
-#' dbGetQuery(conn,"SELECT COUNT(*) FROM test_table")
-#' dbDisconnect(conn)
+#'   conn <- connect(dbms="oracle", server="127.0.0.1:1521/xe",user="system",password="xxx",schema="test")
+#'   dbGetQuery(conn,"SELECT COUNT(*) FROM test_table")
+#'   dbDisconnect(conn)
+#' }
 #' @export
 connect <- function(dbms = "mysql", user, password, server, port, schema){
 	if (dbms == "mysql"){
@@ -62,12 +64,12 @@ connect <- function(dbms = "mysql", user, password, server, port, schema){
 		pathToJar <- system.file("java", "mysql-connector-java-5.1.18-bin.jar", package="DatabaseConnector")
 		driver <- JDBC("com.mysql.jdbc.Driver", pathToJar, identifier.quote="`")
 		connection <- dbConnect(driver, paste("jdbc:mysql://",server,sep=""), user, password)
-		if (!missing(schema))
+		if (!missing(schema) && !is.null(schema))
 			dbSendUpdate(connection,paste("USE",schema))
 		return(connection)
 	}	
 	if (dbms == "sql server"){
-		if (missing(user)) { # Using Windows integrated security
+		if (missing(user) || is.null(user)) { # Using Windows integrated security
 			print("Connecting using SQL Server driver using Windows integrated security")
 			pathToJar <- system.file("java", "sqljdbc4.jar", package="DatabaseConnector")
 			driver <- JDBC("com.microsoft.sqlserver.jdbc.SQLServerDriver", pathToJar)
@@ -83,7 +85,7 @@ connect <- function(dbms = "mysql", user, password, server, port, schema){
 				connection <- dbConnect(driver, paste("jdbc:jtds:sqlserver://",server,sep=""), user, password)
 			}
 		}
-		if (!missing(schema))
+		if (!missing(schema) && !is.null(schema))
 			dbSendUpdate(connection,paste("USE",schema))
 		return(connection)
 	}
@@ -97,7 +99,7 @@ connect <- function(dbms = "mysql", user, password, server, port, schema){
 		
 		# Next, try THIN driver:
 		if (result =="try-error"){
-			if (missing(port))
+			if (missing(port)|| is.null(port))
 				port = "1521"
 			host = "127.0.0.1"	
 			sid = server
@@ -115,7 +117,7 @@ connect <- function(dbms = "mysql", user, password, server, port, schema){
 			print(paste("jdbc:oracle:thin:@",server,":",port,":",sid ,sep=""))
 			connection <- dbConnect(driver, paste("jdbc:oracle:thin:@",host,":",port,":",sid ,sep=""), user, password)
 		}
-		if (!missing(schema))
+		if (!missing(schema) && !is.null(schema))
 			dbSendUpdate(connection,paste("ALTER SESSION SET current_schema = ",schema))
 		return(connection)
 	}
@@ -142,14 +144,16 @@ connect <- function(dbms = "mysql", user, password, server, port, schema){
 #' security, the domain can be specified using <mydomain>/<user>.
 #' @param password			The password for that user
 #' @param server			The name or IP-address of the server
+#' @param port				(optional) The port on the server to connect to
 #' @param schema			(optional) The name of the schema to connect to
 #' @return              
 #' A list with all the details needed to connect to a database.
-#' @examples
-#' connectionDetails <- createConnectionDetails(dbms="mysql", server="localhost",user="root",password="F1r3starter",schema="cdm_v4")
-#' conn <- connectUsingConnectionDetails(connectionDetails)
-#' dbGetQuery(conn,"SELECT COUNT(*) FROM person")
-#' dbDisconnect(conn)
+#' @examples \dontrun{
+#'   connectionDetails <- createConnectionDetails(dbms="mysql", server="localhost",user="root",password="F1r3starter",schema="cdm_v4")
+#'   conn <- connectUsingConnectionDetails(connectionDetails)
+#'   dbGetQuery(conn,"SELECT COUNT(*) FROM person")
+#'   dbDisconnect(conn)
+#' }
 #' @export
 createConnectionDetails <- function(dbms = "mysql", user, password, server, port, schema){
 	return(as.list(match.call()))
@@ -167,13 +171,20 @@ createConnectionDetails <- function(dbms = "mysql", user, password, server, port
 #' @param connectionDetails              a list created by the \code{createConnectionDetails} function.
 #' @return              
 #' An object that extends \code{DBIConnection} in a database-specific manner. This object is used to direct commands to the database engine. 
-#' @examples
-#' connectionDetails <- createConnectionDetails(dbms="mysql", server="localhost",user="root",password="xxx",schema="cdm_v4")
-#' conn <- connectUsingConnectionDetails(connectionDetails)
-#' dbGetQuery(conn,"SELECT COUNT(*) FROM person")
-#' dbDisconnect(conn)
+#' @examples \dontrun{
+#'   connectionDetails <- createConnectionDetails(dbms="mysql", server="localhost",user="root",password="xxx",schema="cdm_v4")
+#'   conn <- connectUsingConnectionDetails(connectionDetails)
+#'   dbGetQuery(conn,"SELECT COUNT(*) FROM person")
+#'   dbDisconnect(conn)
+#' }
 #' @export
 connectUsingConnectionDetails <- function(connectionDetails){
-	do.call("connect",connectionDetails)
+	dbms = connectionDetails$dbms
+	user = connectionDetails$user
+	password = connectionDetails$password
+	server = connectionDetails$server
+	port = connectionDetails$port
+	schema = connectionDetails$schema
+	connect(dbms, user, password, server, port, schema)
 }
 
