6ccb7d0be391de857b39be1e15a69a167759d780
Marc Suchard <msuchard@gmail.com>
2015-06-08 16:13:07 -0700

    Debug travis

--- tests/testthat/test-connection.R
+++ tests/testthat/test-connection.R
@@ -2,12 +2,18 @@ library(testthat)
 
 test_that("Open connection", {
   # Postgresql
-  connection <- connect(
+  connection <- 
+  tryCatch({
+	connect(
     createConnectionDetails(dbms = "postgresql",
                             user = Sys.getenv("CDM5_POSTGRESQL_USER"),
                             password = Sys.getenv("CDM5_POSTGRESQL_PASSWORD"),
                             server = Sys.getenv("CDM5_POSTGRESQL_SERVER"),
                             schema = Sys.getenv("CDM5_POSTGRESQL_SCHEMA")))
+  }, error <- function(e) {
+  	print(traceback())
+  	e
+  })
   expect_true(inherits(connection, "JDBCConnection"))
   expect_true(DBI::dbDisconnect(connection))
 })
