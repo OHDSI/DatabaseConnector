918a8624783d4a457d9ec587f3d178b955df9c9f
schuemie <schuemie@ohdsi.org>
2015-06-12 07:36:41 +0800

    Added check of environmental variables

--- tests/testthat/test-connection.R
+++ tests/testthat/test-connection.R
@@ -1,6 +1,16 @@
 library(testthat)
 
-#test_that("Open connection", {
+test_that("Environmental variables", {
+  expect_equal(Sys.getenv("CDM5_POSTGRESQL_USER"), "ohdsi")
+  expect_equal(Sys.getenv("CDM5_POSTGRESQL_SERVER"), "jenkins.ohdsi.org/CDMV5")
+  expect_equal(Sys.getenv("CDM5_POSTGRESQL_SCHEMA"), "public")
+  pw <- Sys.getenv("CDM5_POSTGRESQL_PASSWORD")
+  checkSum <- sum(as.numeric(charToRaw(pw)))
+  expect_equal(checkSum, 1149)
+  
+})
+
+test_that("Open connection", {
   # Postgresql
   details <-
     createConnectionDetails(dbms = "postgresql",
@@ -11,4 +21,4 @@ library(testthat)
   connection <- connect(details)
   expect_true(inherits(connection, "JDBCConnection"))
 #  expect_true(DBI::dbDisconnect(connection))
-#})
+})
