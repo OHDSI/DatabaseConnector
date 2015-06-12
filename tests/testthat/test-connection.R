91089a1bf48147c49ac0f8f681307f869fa65613
schuemie <schuemie@ohdsi.org>
2015-06-12 08:31:08 +0800

    Using URLencode - URLdecode to get around Travis special character issues.

--- tests/testthat/test-connection.R
+++ tests/testthat/test-connection.R
@@ -1,21 +1,11 @@
 library(testthat)
 
-test_that("Environmental variables", {
-  expect_equal(Sys.getenv("CDM5_POSTGRESQL_USER"), "ohdsi")
-  expect_equal(Sys.getenv("CDM5_POSTGRESQL_SERVER"), "jenkins.ohdsi.org/CDMV5")
-  expect_equal(Sys.getenv("CDM5_POSTGRESQL_SCHEMA"), "public")
-  pw <- Sys.getenv("CDM5_POSTGRESQL_PASSWORD")
-  checkSum <- sum(as.numeric(charToRaw(pw)))
-  expect_equal(checkSum, 1149)
-  
-})
-
 test_that("Open connection", {
   # Postgresql
   details <-
     createConnectionDetails(dbms = "postgresql",
                             user = Sys.getenv("CDM5_POSTGRESQL_USER"),
-                            password = Sys.getenv("CDM5_POSTGRESQL_PASSWORD"),
+                            password = URLdecode(Sys.getenv("CDM5_POSTGRESQL_PASSWORD")),
                             server = Sys.getenv("CDM5_POSTGRESQL_SERVER"),
                             schema = Sys.getenv("CDM5_POSTGRESQL_SCHEMA"))
   connection <- connect(details)
