3c1b727b537fa88fd5aa2bc00e8dfa0f721ed26d
Cornelius Raths <cornelius.raths@nantmobile.com>
2017-04-12 14:41:10 -0700

    Added explicit conversion to R's Date type if database is Impala and dataframe column name contains substring "DATE"

--- R/Sql.R
+++ R/Sql.R
@@ -323,8 +323,15 @@ querySql <- function(connection, sql) {
 
     result <- lowLevelQuerySql(connection, sql)
     colnames(result) <- toupper(colnames(result))
+    if(attr(connection, "dbms") == "impala") {
+      for (colname in colnames(result)) {
+        if(grepl('DATE', colname)) {
+          result[[colname]] <- as.Date(result[[colname]], "%Y-%m-%d")
+        }
+      }
+    }
     return(result)
-  }, error = function(err) {
+    }, error = function(err) {
     writeLines(paste("Error executing SQL:", err))
 
     # Write error report:
@@ -381,6 +388,13 @@ querySql.ffdf <- function(connection, sql) {
   tryCatch({
     result <- lowLevelQuerySql.ffdf(connection, sql)
     colnames(result) <- toupper(colnames(result))
+    if(attr(connection, "dbms") == "impala") {
+          for (colname in colnames(result)) {
+          if(grepl('DATE', colname)) {
+          result[[colname]] <- as.Date(result[[colname]], "%Y-%m-%d")
+        }
+      }
+    }
     return(result)
   }, error = function(err) {
     writeLines(paste("Error executing SQL:", err))
