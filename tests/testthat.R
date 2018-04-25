e20258d06e81bf37131b2281cf76b2a5389b3346
schuemie <schuemie@ohdsi.org>
2018-04-25 04:17:08 -0400

    Preparing for CRAN

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(DatabaseConnector)
-if (!is.null(Sys.getenv("CDM5_POSTGRESQL_SERVER"))) {
+if (Sys.getenv("CDM5_POSTGRESQL_SERVER") != "") {
   test_check("DatabaseConnector")
 } else {
-  writeLines("Skipping testing because environmental variables not set") 
+  message("Skipping testing because environmental variables not set") 
 }
