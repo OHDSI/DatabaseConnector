c2acdc1f02167eefd7d5030069eafa1425ecc960
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2019-11-19 13:56:53 +0100

    added windows functionality for copying file to hadoop

--- R/InsertTable.R
+++ R/InsertTable.R
@@ -647,17 +647,27 @@ insertTable.DatabaseConnectorDbiConnection <- function(connection,
   hiveKeyFile <- (function(keyfile) if (keyfile == "") NULL else keyfile)(Sys.getenv("HIVE_KEYFILE"))
   hadoopUser <- (function(hadoopUser) if (hadoopUser == "") "hive" else hadoopUser)(Sys.getenv("HADOOP_USER_NAME"))
 
-  tryCatch({
-    remoteFile <- paste0("/tmp/", fileName)
-    scp_command <- sprintf("sshpass -p \'%s\' scp -P %s %s %s:%s", hivePasswd, sshPort, filePath, hiveHost, remoteFile)
-    system(scp_command)
-    hadoopDir <- sprintf("/user/%s/%s", hadoopUser, uuid::UUIDgenerate(use.time = TRUE))
-    hadoopFile <- paste0(hadoopDir, "/", fileName)
-    hdp_mk_dir_command <- sprintf("sshpass -p \'%s\' ssh %s -p %s HADOOP_USER_NAME=%s hadoop fs -mkdir %s", hivePasswd, hiveHost, sshPort, hadoopUser, hadoopDir)
-    system(hdp_mk_dir_command)
-    hdp_put_command <- sprintf("sshpass -p \'%s\' ssh %s -p %s HADOOP_USER_NAME=%s hadoop fs -put %s %s", hivePasswd, hiveHost, sshPort, hadoopUser, remoteFile, hadoopFile)
-    system(hdp_put_command)
-
+    tryCatch({
+    if (Sys.info()['sysname'] == 'Windows') {
+        session <- ssh_connect(host = sprintf("%s@%s:%s", hiveUser, hiveHost, sshPort), passwd = hivePasswd, keyfile = hiveKeyFile)
+        remoteFile <- paste0("/tmp/", fileName)
+        scp_upload(session, filePath, to = remoteFile, verbose = FALSE)
+        hadoopDir <- sprintf("/user/%s/%s", hadoopUser, uuid::UUIDgenerate(use.time = TRUE))
+        hadoopFile <- paste0(hadoopDir, "/", fileName)
+        ssh_exec_wait(session, sprintf("HADOOP_USER_NAME=%s hadoop fs -mkdir %s", hadoopUser, hadoopDir))
+        command <- sprintf("HADOOP_USER_NAME=%s hadoop fs -put %s %s", hadoopUser, remoteFile, hadoopFile)
+        ssh_exec_wait(session, command = command)
+    } else {
+        remoteFile <- paste0("/tmp/", fileName)
+        scp_command <- sprintf("sshpass -p \'%s\' scp -P %s %s %s:%s", hivePasswd, sshPort, filePath, hiveHost, remoteFile)
+        system(scp_command)
+        hadoopDir <- sprintf("/user/%s/%s", hadoopUser, uuid::UUIDgenerate(use.time = TRUE))
+        hadoopFile <- paste0(hadoopDir, "/", fileName)
+        hdp_mk_dir_command <- sprintf("sshpass -p \'%s\' ssh %s -p %s HADOOP_USER_NAME=%s hadoop fs -mkdir %s", hivePasswd, hiveHost, sshPort, hadoopUser, hadoopDir)
+        system(hdp_mk_dir_command)
+        hdp_put_command <- sprintf("sshpass -p \'%s\' ssh %s -p %s HADOOP_USER_NAME=%s hadoop fs -put %s %s", hivePasswd, hiveHost, sshPort, hadoopUser, remoteFile, hadoopFile)
+        system(hdp_put_command)
+    }
     def <- function(name) {
       return(paste(name, "STRING"))
     }
@@ -676,7 +686,10 @@ insertTable.DatabaseConnectorDbiConnection <- function(connection,
     }, finally = {
       try(invisible(file.remove(filePath)))
     })
-  })
+  }, finally = {
+    if (Sys.info()['sysname'] == 'Windows')
+        ssh_disconnect(session)
+    })
 }
 
 # Borrowed from devtools:
