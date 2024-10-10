e2b598689e43f7d4c9b3c2fa89b664e7204f13a3
Benjamin De Boe <benjamin.de.boe@intersystems.com>
2024-10-10 13:03:45 +0200

    drop temp table if exists at start of test

--- tests/testthat/test-sendUpdates.R
+++ tests/testthat/test-sendUpdates.R
@@ -1,6 +1,7 @@
 library(testthat)
 
-sql <- "CREATE TABLE #temp (x INT);
+sql <- "IF OBJECT_ID('tempdb..#temp', 'U') IS NOT NULL DROP TABLE #temp;
+    CREATE TABLE #temp (x INT);
     INSERT INTO #temp (x) SELECT 123;
     DELETE FROM #temp WHERE x = 123;
     DROP TABLE #temp;"
@@ -11,8 +12,8 @@ for (testServer in testServers) {
     options(sqlRenderTempEmulationSchema = testServer$tempEmulationSchema)
     on.exit(dropEmulatedTempTables(connection))
     on.exit(disconnect(connection), add = TRUE)
-    expect_equal(renderTranslateExecuteSql(connection, sql), c(0, 1, 1, 0))
-    expect_equal(renderTranslateExecuteSql(connection, sql, runAsBatch = TRUE), c(0, 1, 1, 0))
+    expect_equal(renderTranslateExecuteSql(connection, sql), c(0, 0, 1, 1, 0))
+    expect_equal(renderTranslateExecuteSql(connection, sql, runAsBatch = TRUE), c(0, 0, 1, 1, 0))
     rowsAffected <- dbSendStatement(connection, sql)
     expect_equal(dbGetRowsAffected(rowsAffected), 2)
     dbClearResult(rowsAffected)
