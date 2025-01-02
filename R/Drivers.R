2ac302c6514daa031de60491f1244a663d5471b3
Benjamin De Boe <benjamin.de.boe@intersystems.com>
2025-01-02 17:48:25 +0100

    update IRIS driver, pull from Maven

--- R/Drivers.R
+++ R/Drivers.R
@@ -49,7 +49,7 @@ jdbcDrivers <- new.env()
 #' - Spark (Databricks): V2.6.36
 #' - Snowflake: V3.16.01
 #' - BigQuery: v1.3.2.1003
-#' - InterSystems IRIS: v3.9.0
+#' - InterSystems IRIS: v3.10.2
 #' 
 #' @return Invisibly returns the destination if the download was successful.
 #' @export
@@ -99,7 +99,7 @@ downloadJdbcDrivers <- function(dbms, pathToDriver = Sys.getenv("DATABASECONNECT
     5,spark,DatabricksJDBC42-2.6.36.1062.zip,https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/jdbc/2.6.36/
     6,snowflake,snowflake-jdbc-3.16.1.jar,https://repo1.maven.org/maven2/net/snowflake/snowflake-jdbc/3.16.1/
     7,bigquery,SimbaBigQueryJDBC42-1.3.2.1003.zip,https://storage.googleapis.com/simba-bq-release/jdbc/
-    8,iris,intersystems-jdbc-3.9.0.jar,https://github.com/intersystems-community/iris-driver-distribution/raw/refs/heads/main/JDBC/JDK18/"
+    8,iris,intersystems-jdbc-3.10.2.jar,https://repo1.maven.org/maven2/com/intersystems/intersystems-jdbc/3.10.2/"
   )
   if (dbms == "all") {
     dbms <- jdbcDriverSources$dbms
