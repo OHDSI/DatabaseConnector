1c120ec7c664337b2a65a2dec0c6f8a58480c7a7
Anthony Sena <asena5@its.jnj.com>
2026-05-12 14:59:23 -0400

    Remove inferSchema

--- inst/sql/sql_server/sparkCopy.sql
+++ inst/sql/sql_server/sparkCopy.sql
@@ -2,7 +2,6 @@ COPY INTO @sqlTableName
 FROM 'abfss://@azureContainerName@@azureStorageAccount.dfs.core.windows.net/@fileName'
 FILEFORMAT = CSV
 FORMAT_OPTIONS (
-   'header' = 'true',
-   'inferSchema' = 'true'
+   'header' = 'true'
 )
 COPY_OPTIONS('mergeSchema' = 'true');
